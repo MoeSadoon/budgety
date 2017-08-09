a29df049e0c541e47bf0fe6e74964c254d4de684
Moe Sadoon <msadoon590@gmail.com>
2017-08-09 23:47:22 +0100

    Added input data functions

--- app.js
+++ app.js
@@ -5,22 +5,46 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function() {
-    // some code
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+             }
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
+
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     const ctrlAddItem = () => {
         // 1. Get input data
+        var input = UICtrl.getInput();
+        console.log(input);
+        
         // 2. Add item to the budget controller
         // 3. Add new item to the user interface
         // 4. Calculate the budget
         // 5. Display the budget on the UI
-
-        console.log("adding item");
     }
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem)
 
 
     // Adding to global environment since you can press enter anywhere
